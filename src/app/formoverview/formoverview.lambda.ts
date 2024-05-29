b8acf7f0abdf7bbc7aa8753fdc83fcf73b8cab7f
Wieteke <149399985+WietekeNijmegen@users.noreply.github.com>
2024-05-29 07:42:30 +0200

    feat: base formoverview from resubmit

---
+++ src/app/formoverview/formoverview.lambda.ts
@@ -0,0 +1,44 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { ApiGatewayV2Response, Response } from '@gemeentenijmegen/apigateway-http/lib/V2/Response';
+import { AWS } from '@gemeentenijmegen/utils';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { FormOverviewApiClient } from './FormOverviewApiClient';
+import { FormOverviewRequestHandler } from './formoverviewRequestHandler';
+
+let requestHandler: FormOverviewRequestHandler | undefined = undefined;
+
+async function init() {
+  //TODO: change secret
+  const apiKey = await AWS.getSecret(process.env.MANAGEMENT_API_KEY_SECRET_ARN!);
+  console.log('Loaded API key', apiKey.substring(0, 4));
+  const apiClient = new FormOverviewApiClient({
+    apiKey: apiKey,
+    baseUrl: process.env.FORMOVERVIEW_API_BASE_URL!,
+    timeout: 4000,
+  });
+  const dynamoDBClient = new DynamoDBClient({});
+  requestHandler = new FormOverviewRequestHandler(dynamoDBClient, apiClient);
+}
+
+const initalization = init();
+
+function parseEvent(event: APIGatewayProxyEventV2) {
+  return {
+    cookies: event?.cookies?.join(';') ?? '',
+    reference: event?.queryStringParameters?.reference,
+  };
+}
+
+export async function handler (event: any, _context: any):Promise<ApiGatewayV2Response> {
+  await initalization;
+  try {
+    const params = parseEvent(event);
+    if (!requestHandler) {
+      throw Error('Request handler not initalized!');
+    }
+    return await requestHandler.handleRequest(params);
+  } catch (err) {
+    console.error(err);
+    return Response.error(500);
+  }
+};
